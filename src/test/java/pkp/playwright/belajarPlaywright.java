a2d97ecac5898051df0a49c68afb4e784b0309ab
Aziz Muslim <aziez.musleam@gmail.com>
2024-02-06 15:22:05 +0700

    Add programsbuzz and Move code

--- src/test/java/pkp/playwright/belajarPlaywright.java
+++ src/test/java/pkp/playwright/belajarPlaywright.java
@@ -17,46 +17,6 @@ import java.util.HashMap;
 //@ExtendWith(AllureJunit5.class)
 public class belajarPlaywright {
     @Test
-    @DisplayName("Verify Url in Playwright Java")
-    public void verifyUrl(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
-                page.navigate("http://www.programsbuzz.com/user/login");
-                page.locator("#edit-name").type("Naruto");
-                page.locator("#edit-pass").type("Uzumaki");
-                String currentUrl = page.url();
-                String expectedUrl = "https://www.programsbuzzz.com/user/login";
-                if (currentUrl.equals(expectedUrl)){
-                    System.out.println("URL is correct " + currentUrl);
-                } else {
-                    System.out.println("URL is incorrect. Expected: " + expectedUrl + " , but got: "+ currentUrl);
-                }
-//                System.out.println(currentUrl);
-        browser.close();
-        playwright.close();
-    }
-
-    @Test
-    @DisplayName("Verify Placeholder Text in Playwright Java")
-    public void verifyPlaceholder(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
-        page.navigate("https://www.programsbuzz.com/user/login");
-        Locator searchBar = page.locator("#edit-keys--2");
-        String placeText = searchBar.getAttribute("keys");
-
-        if (placeText != null && placeText.contains("Enter the terms you wish to search for.")) {
-            System.out.println("PASS");
-        } else if (placeText == null || placeText.isEmpty()) {
-            System.out.println("FAIL! no such texts");
-        } else {
-            System.out.println("FAIL! Unexpected content");
-        }
-    }
-
-    @Test
     @DisplayName("Download File in Playwright Java")
     public void downloadFileTest() {
         Playwright playwright = Playwright.create();
@@ -159,6 +119,7 @@ public class belajarPlaywright {
 
         JsonObject j = new Gson().fromJson(response, JsonObject.class);
         System.out.println(j.get("name"));
+        System.out.println(j.get("job"));
 
         page.close();
         browser.close();
---
+++ src/test/java/pkp/playwright/programsbuzz/programsbuzz.java
@@ -0,0 +1,47 @@
+package pkp.playwright.programsbuzz;
+
+import com.microsoft.playwright.*;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class programsbuzz {
+    @Test
+    @DisplayName("Verify Url in Playwright Java")
+    public void verifyUrl(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://www.programsbuzz.com/user/login");
+        page.locator("#edit-name").type("Naruto");
+        page.locator("#edit-pass").type("Uzumaki");
+        String currentUrl = page.url();
+        String expectedUrl = "https://www.programsbuzzz.com/user/login";
+        if (currentUrl.equals(expectedUrl)){
+            System.out.println("URL is correct " + currentUrl);
+        } else {
+            System.out.println("URL is incorrect. Expected: " + expectedUrl + " , but got: "+ currentUrl);
+        }
+//                System.out.println(currentUrl);
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    @DisplayName("Verify Placeholder Text in Playwright Java")
+    public void verifyPlaceholder(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://www.programsbuzz.com/user/login");
+        Locator searchBar = page.locator("#edit-keys--2");
+        String placeText = searchBar.getAttribute("keys");
+
+        if (placeText != null && placeText.contains("Enter the terms you wish to search for.")) {
+            System.out.println("PASS");
+        } else if (placeText == null || placeText.isEmpty()) {
+            System.out.println("FAIL! no such texts");
+        } else {
+            System.out.println("FAIL! Unexpected content");
+        }
+    }
+}
